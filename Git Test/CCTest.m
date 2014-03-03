ba90c67690b780dff9a2cd5b1a6ed2d78ae157f8
Sanaah Al Salami <sanaahalsalami@Sanaahs-MacBook-Air.local>
2014-03-03 00:06:20 -0500

    added a test class

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Sanaah Al Salami on 3/3/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
